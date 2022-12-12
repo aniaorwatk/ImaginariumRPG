42a8409d713ee1364e0ec580fd4ad9b003e45bec
ania orwatk <orwat.ania@gmail.com>
2022-12-12 12:48:02 +0100

    test json server

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
 
-const App =() => {
+interface IAppType {
+  title: string
+}
+
+const App = () => {
+
+  const URL = "http://localhost:3000/test"
+
+  const [test, setTest] = useState<IAppType[]>([])
+
+  useEffect(() => {
+    fetch(URL, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setTest(res);
+        console.log(test)
+      })
+
+      .catch((err) => console.error(err));
+  }, []);
+
+
   return (
     <div className="App">
       <h1>Imaginarium</h1>
+      {test.map(one => {
+        return (
+          one.title
+        )
+      })}
     </div>
   );
 }
