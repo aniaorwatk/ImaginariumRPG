62d2ea5dcc8d35b3419a9b3c03e58e7d3e701fa9
ania orwatk <orwat.ania@gmail.com>
2022-12-13 11:02:05 +0100

    add data media

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
+import { Icon } from '@iconify/react';
 
 interface IAppType {
   title: string
 }
+interface IMediaTyp {
+  title: string,
+  logo: string,
+  link: string,
+  color: string,
+  colorTwo: string
+}
 
 const App = () => {
 
-  const URL = "http://localhost:3000/test"
+  const URL_TEST = "http://localhost:3000/test"
+  const URL_MEDIA = "http://localhost:3000/media"
 
   const [test, setTest] = useState<IAppType[]>([])
+  const [media, setMedia] = useState<IMediaTyp[]>([])
 
   useEffect(() => {
-    fetch(URL, {
+    fetch(URL_TEST, {
       method: "GET",
     })
       .then((res) => res.json())
@@ -23,6 +33,31 @@ const App = () => {
       .catch((err) => console.error(err));
   }, []);
 
+  useEffect(() => {
+    fetch(URL_MEDIA, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setMedia(res);
+        console.log(media)
+      })
+
+      .catch((err) => console.error(err));
+  }, []);
+
+  const allMedia = media.map(one => {
+    return (
+      <div>
+        <Icon icon={one.logo} 
+        color={one.color} 
+        width="40" height="40"
+  />
+        <p>{one.title}</p>
+      </div>
+    )
+  })
+
 
   return (
     <div className="App">
@@ -32,6 +67,7 @@ const App = () => {
           one.title
         )
       })}
+      {allMedia}
     </div>
   );
 }
