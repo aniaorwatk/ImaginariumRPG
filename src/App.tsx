c710265865c242684526645a406e4bcbe32e7c33
ania orwatk <orwat.ania@gmail.com>
2022-12-13 13:00:48 +0100

    add data patronite, events,banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
 import { Icon } from '@iconify/react';
+import { ReactComponent as LogoPatronite } from "./assets/patronite.svg"
+import "./index.css"
 
-interface IAppType {
-  title: string
-}
 interface IMediaTyp {
   title: string,
   logo: string,
@@ -12,26 +11,57 @@ interface IMediaTyp {
   colorTwo: string
 }
 
+interface IPatroniteType {
+  email: string
+}
+interface IBannerType {
+  scr_banner: string
+}
+interface IEventsType {
+  id: number,
+  title: string,
+  src_event: string
+}
+
 const App = () => {
 
-  const URL_TEST = "http://localhost:3000/test"
-  const URL_MEDIA = "http://localhost:3000/media"
 
-  const [test, setTest] = useState<IAppType[]>([])
+  const URL_MEDIA = "http://localhost:3000/media"
+  const URL_PATRONITE = "http://localhost:3000/patronite"
+  const URL_BANNER = "http://localhost:3000/banner"
+  const URL_EVENTS = "http://localhost:3000/events"
+
   const [media, setMedia] = useState<IMediaTyp[]>([])
+  const [patronite, SetPatronite] = useState<IPatroniteType>()
+  const [banner, setBanner] = useState<IBannerType>()
+  const [events, setEvents] = useState<IEventsType[]>([])
 
   useEffect(() => {
-    fetch(URL_TEST, {
+    fetch(URL_BANNER, {
       method: "GET",
     })
       .then((res) => res.json())
       .then((res) => {
-        setTest(res);
-        console.log(test)
+        setBanner(res);
+        
       })
 
       .catch((err) => console.error(err));
   }, []);
+
+  useEffect(() => {
+    fetch(URL_EVENTS, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setEvents(res);
+      
+      })
+
+      .catch((err) => console.error(err));
+  }, []);
+
 
   useEffect(() => {
     fetch(URL_MEDIA, {
@@ -40,7 +70,18 @@ const App = () => {
       .then((res) => res.json())
       .then((res) => {
         setMedia(res);
-        console.log(media)
+      })
+
+      .catch((err) => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    fetch(URL_PATRONITE, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        SetPatronite(res);
       })
 
       .catch((err) => console.error(err));
@@ -48,26 +89,40 @@ const App = () => {
 
   const allMedia = media.map(one => {
     return (
-      <div>
-        <Icon icon={one.logo} 
-        color={one.color} 
-        width="40" height="40"
-  />
+      <div key={one.title}>
+        <Icon icon={one.logo}
+          color={one.color}
+          width="40" height="40"
+        />
         <p>{one.title}</p>
       </div>
     )
   })
 
+  const allEvents = events.map(event => {
+    return (
+      <div key={event.id}>
+        <img src={event.src_event} alt={event.title} />
+        <p>{event.title}</p>
+      </div>
+    )
+  })
+
+
+
 
   return (
     <div className="App">
       <h1>Imaginarium</h1>
-      {test.map(one => {
-        return (
-          one.title
-        )
-      })}
-      {allMedia}
+      {patronite?.email && <><LogoPatronite /><>{patronite?.email}</></>}
+      <div className="media">
+        {allMedia}
+      </div>
+      <div className="events">
+        {allEvents}
+      </div>
+      <img src={banner?.scr_banner} alt="banner" className="banner"/>
+
     </div>
   );
 }
