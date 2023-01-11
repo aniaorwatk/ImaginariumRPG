a48669ab667eb28cbf3ff766895a26cf6975ee7b
ania orwatk <orwat.ania@gmail.com>
2023-01-11 14:28:59 +0100

    clean commit

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -6,24 +6,8 @@ import arrowIcon from "./../../assets/arrow_icon.svg";
 import "./Events.css";
 
 const Events = () => {
-
-
-
     const scrl = useRef<HTMLDivElement>({});
 
-
-const [test,setTest]=useState()
-
-
-
-
-    let scrlCurrent = scrl.current?.scrollWidth as number
-    let scrlOffW = scrl.current?.offsetWidth as number
-
-
-
-
-    // DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
     const [events, setEvents] = useState<IEventType[]>([])
     const [arrow, setArrow] = useState(false)
     const [scrollX, setscrollX] = useState(0)
@@ -56,117 +40,38 @@ const [test,setTest]=useState()
         )
     }
 
-    // const arrowRef =useRef<HTMLDivElement>(null)
-    // const arrowContainerRef=useRef<HTMLDivElement>(null)
-
-
-    // useEffect(()=>{
-    //     const eventBoxWidth = arrowRef.current?.getBoundingClientRect().width
-    //     console.log(eventBoxWidth)
-    //      console.log(arrowContainerRef.current?.getBoundingClientRect().width) 
-    //     if(null !== arrowContainerRef.current){
-
-    //     }
-    // },[arrowContainerRef])
-
-
-
-    //    useEffect(() => {
-    //     //Check width of the scollings
-    //     if (
-    //       scrl.current &&
-    //       scrl?.current?.scrollWidth === scrl?.current?.offsetWidth
-    //     ) {
-    //       setscrolEnd(true);
-    //     } else {
-    //       setscrolEnd(false);
-    //     }
-    //     return () => {};
-    //   }, [scrl?.current?.scrollWidth, scrl?.current?.offsetWidth]);
-
-    let sLeft = scrl.current?.scrollLeft as number
-    // const slide = (shift: number) => {
-        
-
-    //     sLeft += shift;
-
-
-    //     setscrollX(scrollX + shift);
-    //     if (
-    //         Math.floor(scrlCurrent - sLeft) <=
-    //         scrlOffW
-    //     ) {
-    //         setscrolEnd(true);
-    //     } else {
-    //         setscrolEnd(false);
-    //     }
-    // };
-
-    // const scrollCheck = () => {
-    //     setscrollX(sLeft);
-    //     if (
-    //         Math.floor(scrlCurrent - sLeft) <=
-    //         scrlOffW
-    //     ) {
-    //         setscrolEnd(true);
-    //     } else {
-    //         setscrolEnd(false);
-    //     }
-    // }
-
-//     useEffect(() => {
-//         console.log(scrlCurrent)
-//         const scrl = useRef<HTMLDivElement>();
-
-//         const setScrl =()=>{
-//             return(
-// setTest(scrl)
-//             )
-//         }
-//         setScrl()
-//     }, [])
-
-    const slide = (shift:number) => {
+    const slide = (shift: number) => {
 
         scrl.current.scrollLeft += shift;
 
         setscrollX(scrollX + shift);
-    
+
         if (
-          Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
-          scrl.current.offsetWidth
+            Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
+            scrl.current.offsetWidth
         ) {
-          setscrolEnd(true);
+            setscrolEnd(true);
         } else {
-          setscrolEnd(false);
+            setscrolEnd(false);
         }
-      };
+    };
 
+    useEffect(() => {
+        console.log(scrollX)
+    }, [scrollX])
 
-
-
-     useEffect(()=>{
-console.log(scrollX)
-     },[scrollX])
-    
-   
-    
-      const scrollCheck = () => {
+    const scrollCheck = () => {
         setscrollX(scrl.current.scrollLeft);
         if (
-          Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
-          scrl.current.offsetWidth
+            Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
+            scrl.current.offsetWidth
         ) {
-          setscrolEnd(true);
+            setscrolEnd(true);
         } else {
-          setscrolEnd(false);
+            setscrolEnd(false);
         }
-      };
+    };
 
-
-
-
-    // console.log(scrl)
     return (
 
         events &&
@@ -175,13 +80,12 @@ console.log(scrollX)
             <div className="events__publishingHouse">
                 <LogoPublishingHouse className="events__publishingHouse-img" />
             </div>
-            {!scrolEnd && 
-           <img src={arrowIcon} alt="arrow right" className={`events__scroll arrowRight   ${arrow ? "arrowShow" : ""}`} onClick={() => slide(+150)}  onTouchMove={() => slide(+150)}/> 
-           } 
-             { scrollX !== 0 &&  
-         <img src={arrowIcon} alt="arrow left" className={`events__scroll arrowLeft   ${arrow ? "arrowShow" : ""}`} onClick={() => slide(-150)} onTouchMove={() => slide(-150)}/> 
-          } 
-
+            {!scrolEnd &&
+                <img src={arrowIcon} alt="arrow right" className={`events__scroll arrowRight   ${arrow ? "arrowShow" : ""}`} onClick={() => slide(+150)} onTouchMove={() => slide(+150)} />
+            }
+            {scrollX !== 0 &&
+                <img src={arrowIcon} alt="arrow left" className={`events__scroll arrowLeft   ${arrow ? "arrowShow" : ""}`} onClick={() => slide(-150)} onTouchMove={() => slide(-150)} />
+            }
             <div className="events" ref={scrl} onScroll={scrollCheck}  >
 
                 {allEvents}
